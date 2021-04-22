d823eb5264901a2c496601595361063c0cc98458
Charles Ferguson <gerph@gerph.org>
2021-04-22 22:27:22 +0100

    Report how well the build went.
    The build-all-apis.sh script now reports how all the builds went -
    they number of passes and fails.

--- build-all-apis.sh
+++ build-all-apis.sh
@@ -17,18 +17,25 @@ tmp=/tmp/oslib_parser.$$.log
 
 OSLibSource=ro-oslib-code/!OSLib/Source
 
+artifactdir="artifacts/CPython"
+npass=0
+nfail=0
 for section in Computer Core User Toolbox ; do
-
     # Build the RISC OS Python interfaces for Chris
-    target="artifacts/CPython/${section}"
+    target="${artifactdir}/${section}"
     mkdir -p "${target}"
     for def in "${OSLibSource}/${section}/oslib/"*.swi ; do
         name=$(basename "$def" .swi)
         echo "+++ Processing ${section}/${name}"
         if ! python oslib_parser.py $def --create-python-api-template "${target}/${name}.py" 2>&1 | tee "$tmp" ; then
             mv "$tmp" "${target}/${name}-failed.log"
+            nfail=$((nfail + 1))
+        else
+            npass=$((npass + 1))
         fi
     done
 done
+echo "--------"
+echo "Finished generating for CPython: Pass=$npass  Fail=$nfail"
 
 rm -f "$tmp"
